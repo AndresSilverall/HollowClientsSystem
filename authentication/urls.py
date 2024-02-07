ef7b8c7f29da91fbc759d03bf683f14634c1d995
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-02-07 17:16:14 -0500

    se implemento el footer del home page

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', view=views.home_page, name="home"),
     
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
