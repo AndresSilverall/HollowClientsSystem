76913d88e785ec389179e6f9bfeb2e2a4b54211c
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-02-21 17:17:25 -0500

    se conecto y se configuro la base de datos MySQL

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import sys
 import os
 import mimetypes
+# from dotenv import load_dotenv
 
 
 mimetypes.add_type("text/css", ".css", True)
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'HOST': os.getenv('DB_HOST'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
