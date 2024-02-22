9dd22b7cb7e663c2a181af6769b16278d7de69dc
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-02-22 16:38:02 -0500

    se agrego los formularios de registro de usuario y login de usuario

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from authentication.forms import CreateNewUser
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 # Vistas de autenticacion.
@@ -7,11 +10,44 @@ def home_page(request):
 
 
 def user_registration(request):
-    return render(request, "register.html")
+
+    form = CreateNewUser()
+
+    if request.method == "POST":
+        form = CreateNewUser(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            form.save()
+            messages.success(request, "Usuario registrado con exito!")
+        else:
+            messages.error(request, "Error, Datos no coinciden!")
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "register.html", context=context)
 
 
 def user_login(request):
-    return render(request, "login.html")
+
+    form = CreateNewUser()
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request,"Error, usuario o contraseña incorrecta!")
+
+    context = {
+        "form": form
+    }
+
+    return render(request, "login.html", context=context)
 
 
 def user_logout(request):
