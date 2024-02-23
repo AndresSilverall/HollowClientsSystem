0463620fc87cd350ebf3bea2430282119f4290a6
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-02-23 12:20:46 -0500

    se implemento ajax en el login form para evitar que se refresque al hacer submit

---
+++ authentication/static/js/login_form.js
@@ -0,0 +1,25 @@
+(function() {
+
+  // Aplicando AJAX al formulario de login para evitar que se refresque la pagina.
+$(document).on("submit", "#login-form", function(e) {
+  e.preventDefault();
+  console.log("se clickeo")
+  $.ajax({
+      type: 'POST',
+      url:  '/login/',
+      data: {
+          username: $("input[name='username']").val(),
+          password: $("input[name='password']").val(),
+          csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
+      },
+      success: function(data) {
+        // Eliminar los datos de entrada de los inputs cuando se hace submit. 
+        $("input[name='username']").val("")
+        $("input[name='password']").val("")
+        
+      }
+
+  });
+});
+
+})();
