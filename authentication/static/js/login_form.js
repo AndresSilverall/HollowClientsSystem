2333db6651046a88dd5bb3641352ebbb1086871e
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-02-23 15:45:49 -0500

    ajax corregido

--- authentication/static/js/login_form.js
+++ authentication/static/js/login_form.js
@@ -1,25 +1,28 @@
-(function() {
-
-  // Aplicando AJAX al formulario de login para evitar que se refresque la pagina.
+// Aplicando AJAX al formulario de login para evitar que se refresque la pagina.
 $(document).on("submit", "#login-form", function(e) {
-  e.preventDefault();
-  console.log("se clickeo")
-  $.ajax({
-      type: 'POST',
-      url:  '/login/',
-      data: {
-          username: $('#username').val(),
-          password: $('#password').val(),
-          csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
-      },
-      success: function(data) {
+    e.preventDefault();
+    console.log("se clickeo")
+    $.ajax({
+        type: 'POST',
+        url:  "/login/",
+        data: {
+            username: $('#username').val(),
+            password: $('#password').val(),
+            csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
+        },
+        success: function(data) {
         // Eliminar los datos de entrada de los inputs cuando se hace submit. 
         $("input[name='username']").val("")
         $("input[name='password']").val("")
+        $("#success").html(data)
         
       }
 
-  });
-});
+      });
 
-})();
+  });
+
+
+  const preventRefreshLoginForm = () => {
+    return null;
+  }
