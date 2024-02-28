35502e95fb7ad96472c352bfcb421a32a79c2262
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-02-28 18:21:18 -0500

    se agrego el modelo para la app de tasks

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class TasksManagement(models.Model):
+
+    class Priority(models.TextChoices):
+        NORMAL = "N", "NORMAL"
+        MEDIANA = "M", "MEDIANA"
+        ALTA = "A", "ALTA"
+
+
+    name = models.CharField("task name", max_length=20, null=False)
+    author = models.CharField("author", max_length=20, null=False)
+    description = models.TextField("description", max_length=50)
+    priority = models.CharField(max_length=10, choices=Priority.choices, default=Priority.NORMAL)
+    created_at = models.DateField("date", null=True)
+
+
+    def __str__(self):
+        return self.name
