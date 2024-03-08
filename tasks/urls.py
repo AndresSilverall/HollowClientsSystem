fe524906e22781c87a15721b82b384bd1306f3e7
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-03-08 17:00:44 -0500

    se agrego la funcion de actualizar una tarea

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,8 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('tasks/', view=views.task_manager, name="tasks"),
-    path('delete/<str:pk>', view=views.delete_task, name="delete"),
     path('add/', view=views.add_task, name="add"),
+    path('update/<str:pk>', view=views.update_task, name="update"),
+    path('delete/<str:pk>', view=views.delete_task, name="delete"),
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- tasks/views.py
+++ tasks/views.py
@@ -19,7 +19,6 @@ def delete_task(request, pk: None):
     if request.method == "POST":  
         tasks = TasksManagement.objects.get(id=pk)
         tasks.delete()
-        messages.success(request, "Tarea eliminada con exito!")
     return redirect("tasks")
 
 
@@ -47,5 +46,22 @@ def add_task(request):
 
 
 #Vista para actualizar una tarea
-def update_task(request):
+def update_task(request, pk: None):
+    if request.method == "POST":
+        tasks = TasksManagement.objects.get(id=pk)
+        user_data = {
+            "title": request.POST.get("taskName"),
+            "description": request.POST.get("taskDescription"),
+            "priority": request.POST.get("taskPriority"),
+            "status": request.POST.get("taskStatus"),
+            "due_date": request.POST.get("dueDate"),
+        }
+        tasks.title = user_data.get("title")
+        tasks.description = user_data.get("description")
+        tasks.priority = user_data.get("priority")
+        tasks.status = user_data.get("status")
+        tasks.due_date = user_data.get("due_date")
+        tasks.save()
+    
+
     return redirect("tasks")
