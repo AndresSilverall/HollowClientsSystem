cfdd5f6078ced9b076ce95144a44408d5e8a8026
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-03-27 21:20:37 -0500

    se agrego pills a las filas de los estados de las tareas

--- menu/views.py
+++ menu/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from tasks.models import TasksManagement
+from contacts.models import CustomersManagement
 
 
 # Vista del menu principal
 def dashboard(request):
+
+    # Estadisticas de la gestion de area
     tasks = TasksManagement.objects.filter(status="en proceso").count()
     tasks_done = TasksManagement.objects.filter(status="Finalizada").count()
+
+    # Estadisticas de la gestion de contactos y campañas de marketing
+    customers = CustomersManagement.objects.count()
+    customers_marketing_campaings = CustomersManagement.objects.filter(campaing="Sin campaña").count()
     context = {
         "tasks": tasks,
-        "is_finished": tasks_done
+        "is_finished": tasks_done,
+        "customers": customers,
+        "customers_campaing": customers_marketing_campaings
     }
     return render(request, "dashboard.html", context=context)
