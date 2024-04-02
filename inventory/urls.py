dde861a9d39fc4d5c325e03c8ac89e71c81b67e9
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-04-02 10:57:18 -0500

    se agrego las diferentes categorias al control de inventario

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,5 +6,13 @@ from . import views
 
 urlpatterns = [
     path('inventory/', views.inventory, name="inventory"),
+    path('brands/', views.brands, name="brands"),
+    path('store/', views.stores, name="stores"),
+    path('orders/', views.orders, name="orders"),
+    path('category/', views.categories, name="category"),
+    path('products/', views.products, name="products"),
+    path('addBrand/', views.add_new_brand, name="add_brand"),
+    path('deleteBrand/<str:pk>', views.delete_brand, name="delete_brand"),
+    path('updateBrand/<str:pk>', views.update_brand, name="update_brand"),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,73 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from inventory.models import (
+    Product,
+    Category,
+    Brand,
+    Store,
+    Order
+)
+
 
 # Vista del control de inventario
 def inventory(request):
     return render(request, "inventory.html")
+
+
+# vista para las tiendas
+def stores(request):
+    return render(request, "store.html")
+
+
+# Vista para las categorias
+def categories(request):
+    return render(request, "category.html")
+
+
+# Vista para las marcas
+def brands(request):
+    brand = Brand.objects.all()
+    context = {
+        "brands": brand 
+    }
+    return render(request, "brands.html", context=context)
+
+
+# Vista para las ordenes
+def orders(request):
+    return render(request, "orders.html")
+
+
+# Vista para los productos
+def products(request):
+    return render(request, "products.html")
+
+
+# -------------------------- CRUD para las diferentes marcas, categorias, tiendas  -----------------------
+
+def add_new_brand(request):
+    if request.method == "POST":
+        brand = Brand.objects.create(
+            name = request.POST.get("brandName"),
+            status = request.POST.get("brandStatus")
+        )
+        brand.save()
+        messages.success(request, "Marca agregada con exito!")
+    return redirect("brands")
+
+
+def delete_brand(request, pk: None):
+    if request.method == "POST":
+        brand = Brand.objects.get(id=pk)
+        brand.delete()
+    return redirect("brands")
+
+
+def update_brand(request, pk=None):
+    if request.method == "POST":
+        brand = Brand.objects.get(id=pk)
+        brand.name = request.POST.get("brandName")
+        brand.status = request.POST.get("brandStatus")
+        brand.save()
+        messages.success(request, "Marca actualizada con exito!")
+    return redirect("brands")
