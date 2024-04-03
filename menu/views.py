e9b071888b968c4dce53f105f6b0d241e24672dc
Andres F. Silvera <andresfsilverall0109@gmail.com>
2024-04-03 10:47:45 -0500

    se agrego las estadisticas de tickets y control de inventario

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from tasks.models import TasksManagement
 from contacts.models import CustomersManagement
+from tickets.models import Tickets
+from inventory.models import (
+    Product,
+    Order,
+    Brand,
+    Category,
+    Store
+)
 
 
 # Vista del menu principal
@@ -9,6 +17,19 @@ def dashboard(request):
     # Estadisticas de la gestion de area
     tasks = TasksManagement.objects.filter(status="en proceso").count()
     tasks_done = TasksManagement.objects.filter(status="Finalizada").count()
+
+    # Estadisticas del control de inventario
+    orders = Order.objects.count()
+    products = Product.objects.count()
+    brands = Brand.objects.count()
+    categories = Category.objects.count()
+    stores = Store.objects.count()
+
+    # Estadisticas del control de los tickets
+    all_tickets = Tickets.objects.count()
+    closed_tickets= Tickets.objects.filter(status="Cerrado").count()
+    open_tickets= Tickets.objects.filter(status="Abierto").count()
+    print(all_tickets, closed_tickets, open_tickets)
 
     # Estadisticas de la gestion de contactos y campañas de marketing
     customers = CustomersManagement.objects.count()
@@ -21,6 +42,14 @@ def dashboard(request):
         "customers": customers,
         "active_customers": active_customers,
         "customers_campaing": customers_marketing_campaings,
-        "no_marketing_campaing": no_marketing_campaings
+        "no_marketing_campaing": no_marketing_campaings,
+        "orders": orders,
+        "products": products,
+        "brands": brands,
+        "categories": categories,
+        "stores": stores,
+        "tickets": all_tickets,
+        "closed_tickets": closed_tickets,
+        "open_tickets": open_tickets
     }
     return render(request, "dashboard.html", context=context)
